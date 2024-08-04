164eb92dc5f5a52c103cce9211212311061af6f6
Brisklemonade <kamellperry33@gmail.com>
2024-08-04 10:40:51 -0500

    add metadata title template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from 'next/image'
+import config from '../../app.config'
 import { Section } from '@/components/layout'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: `Home | ${config.name}`,
+  description: '',
+}
 
 export default function Home() {
   return (
