85199f86c044ae44687a347402349285075babfa
Brisklemonade <kamellperry33@gmail.com>
2024-08-21 14:47:54 -0500

    Remove radix themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-import '@radix-ui/themes/styles.css' // If you don't want to use radix themes delete this
 import '@/styles/globals.css'
 import { config } from 'app-config'
 import { cn } from '@/lib'
-import { Theme } from '@radix-ui/themes'
 import { Inter as FontSans } from 'next/font/google'
 import { Header, Footer, PageLayout } from '@/components/global'
 import type { Metadata } from 'next'
@@ -28,11 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        <Theme className="bg-background text-primary">
-          <Header />
-          <PageLayout>{children}</PageLayout>
-          <Footer />
-        </Theme>
+        <Header />
+        <PageLayout>{children}</PageLayout>
+        <Footer />
       </body>
     </html>
   )
