e8fe422eaff186395447f2657511a0c96f42599a
Brisklemonade <kamellperry33@gmail.com>
2024-08-21 20:41:59 -0500

    Update app config and it's type

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,12 @@ import { AppConfig } from '@/types';
 
 const config: AppConfig = {
     name: 'New Project',
+    url: "https://nextjs.org/",
+    ogImage: "https://nextjs.org/og.jpg",
+    description: "This is a new NextJS project.",
     routes: [
-        { title: "Home", url: "/" },
-        { title: "Test Link 2", url: '/#' }
+        { title: "Home", href: "/" },
+        { title: "Test Link 2", href: '/#' }
     ],
     paths: {
         logo: 'logo.png'
--- src/types/AppConfig.ts
+++ src/types/AppConfig.ts
@@ -1,12 +1,15 @@
 export interface AppConfig {
     name: string;
+    url: `https://${string}.com`,
+    ogImage: `https://${string}`,
+    description: string;
     routes: Route[];
     paths: Paths;
 }
 
 export interface Route {
     title: string;
-    url: `/${string}`;
+    href: `/${string}`;
 }
 
 interface Paths {
