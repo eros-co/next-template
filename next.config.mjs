48b42236172e57a487fe6130c8338d53e4f037e3
Brisklemonade <kamellperry33@gmail.com>
2024-08-22 17:14:48 -0500

    Update and fix nextconfig typescript options

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // !! WARN !!
-    // ignoreBuildErrors: true, // Dangerously allow production builds to successfully complete even if your project has type errors.
-    // !! WARN !!
+    typescript: {
+        // !! WARN !! //
+        // ignoreBuildErrors: true, /* Dangerously allow production builds to successfully complete even if your project has type errors. */
+        // !! WARN !! //
+    },
     reactStrictMode: true,
     swcMinify: true,
     experimental: {
