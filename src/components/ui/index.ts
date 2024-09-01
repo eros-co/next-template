4a2092d8a2cc3b0069b504f45f9766f46513afa7
Brisklemonade <kamellperry33@gmail.com>
2024-09-01 15:24:35 -0500

    Export default components and begin adding export comments for all shadcn components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,16 @@
-export { };
+/** Named exports */
+export { Text, textVariants, type TextProps } from './text';
+export { Heading, headingVariants, type HeadingProps } from './heading';
+export { Button, buttonVariants, type ButtonProps } from './button';
+
+/** Aggregated module exports */
+
+// export * as Accordion from './accordion';
+// export * as Avatar from './avatar';
+// export * as Card from './card';
+// export * as Collapsible from './collapsible';
+// export * as Drawer from './drawer';
+// export * as Popover from './popover';
+// export * as Select from './select';
+// export * as Sheet from './sheet';
+// export * as Tabs from './tabs';
