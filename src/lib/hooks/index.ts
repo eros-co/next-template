54daca769bcb40090a0e0de29ed6daa76e0ad97f
Brisklemonade <kamellperry33@gmail.com>
2024-10-04 18:42:29 -0500

    Removed old hooks

--- src/lib/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useScroll } from './use-scroll';
-export { useMediaQuery, type Device, type Dimensions } from './use-media-query';
-export { useLocalStorage } from './use-local-storage';
-export { useIO, type Args as IOArgs } from './use-intersection-observer';
