a526ceb791b23f06776eb37ab136415424d9db25
Brisklemonade <kamellperry33@gmail.com>
2024-10-04 18:55:23 -0500

    remove experimental option from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
     typescript: {
         // !! WARN !! //
         // ignoreBuildErrors: true, /* Dangerously allow production builds to successfully complete even if your project has type errors. */
         // !! WARN !! //
-    },
-    reactStrictMode: true,
-    swcMinify: true,
-    experimental: {
-        optimizePackageImports: [ '@mantine/hooks' ],
     },
 };
 
