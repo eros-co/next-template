54c49430b5def632d22ea12b864d63e13c7f9dee
Brisklemonade <kamellperry33@gmail.com>
2024-10-22 15:31:35 -0500

    Add company info property to app config and it's types

--- src/lib/types/AppConfig.ts
+++ src/lib/types/AppConfig.ts
@@ -1,4 +1,7 @@
 type DomainSuffix = 'com' | 'org' | 'net' | 'dev' | 'app' | 'io' | 'ai' | 'co';
+type CountryCode = '1';
+type PhoneNumber = `+${CountryCode}${number}${number}${number}-${number}${number}${number}-${number}${number}${number}${number}`;
+type Email = `${string}@${string}.${DomainSuffix}`;
 
 export interface AppConfig {
     name: string;
@@ -7,7 +10,23 @@ export interface AppConfig {
     description: string;
     routes: Route[];
     paths: Paths;
+    company?: CompanyInformation;
 }
+
+interface CompanyInformation {
+    phone: Record<string, PhoneNumber>;
+    email: Record<string, Email>;
+    socials?: {
+        x?: `https://www.x.com/${string}/`,
+        instagram?: `https://www.instagram.com/${string}/`,
+        facebook?: `https://www.facebook.com/${string}/`,
+        linkedin?: `https://www.linkedin.com/${string}/`,
+        youtube?: `https://www.youtube.com/${string}/`,
+        tiktok?: `https://www.tiktok.com/${string}/`,
+        github?: `https://www.github.com/${string}/`,
+    };
+}
+
 
 export interface Route {
     title: string;
